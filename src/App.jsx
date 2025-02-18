44e875f40fd741ce6a849f30f0376017cefdbaee
mahesh-pagrut <maheshdpmayshu@gmail.com>
2025-02-19 01:58:21 +0530

    initial setup and debug tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+import React from 'react'
+
+export default function() {
+  return (
+    <div className='bg-gray-900 h-screen flex justify-center items-center'>
+
+      <h1 className='text-white  text-2xl font-bold'>hi my name is mahesh</h1>
+    </div>
+  )
+}
